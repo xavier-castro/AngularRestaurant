dc4f4c378dffe1641a25df9b6da509dafccc3d52
xavier-castro <xavier_castro@icloud.com>
2019-01-31 22:09:44 -0600

    Recipe model created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes = [];
 
   constructor() { }
 
